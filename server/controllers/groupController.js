a969678645b599bc77cd8543e896d4aecf558c11
Abaas Khorrami <abaas.khorrami@gmail.com>
2020-03-11 10:47:04 -0400

    authController complete - groupController sudo code
    Co-authored-by: Natalie Vick <vicknatalie@gmail.com>

---
+++ server/controllers/groupController.js
@@ -0,0 +1,17 @@
+// ALGO LOGIC! WHOO!
+
+// base case: if form is not filled out
+
+// user high priority: both put each other as 1
+
+// weighting:
+// .5x
+
+// we must have a balanced team based on tech stack
+  // ex: 4 people, 2 FE, 2 BE are broken up
+// we must have balanced team based on skill level
+  // ex: 4 people, a 2 and an 8 paired, and a 3 and a 5 paired
+// we must have teams of people that want to work most together
+  // ex: 4 people. no one gives top/bottom preferences, then random
+    // 2 people top pick each other, & two others w/ each other, then matched
+    // 
--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const authController = require('./controllers/authController.js');
 
 const port = 3000;
 
 const app = express();
+
+app.use(bodyParser());
 
 app.get('/', (req, res) => {
   console.log('/ endpoint hit')
@@ -14,5 +18,25 @@ app.get('/dist/bundle.js', (req, res) => {
   console.log('dist endpoint hit');
   res.sendFile(path.resolve(__dirname, 'dist/bundle.js'));
 })
+
+app.post('/signup', authController.userSignup, authController.getUserData, (req, res) => {
+  res.status(200).json(res.locals.userData);
+});
+
+app.post('/login', authController.getUserData, (req, res) => {
+  res.status(200).json(res.locals.userData);
+});
+
+app.use((req, res) => res.sendStatus(404));
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Unknown error caught in middleware',
+    status: '400',
+    message: err
+  };
+  console.log(defaultErr);
+  res.status(defaultErr.status).json(defaultErr.message);
+});
 
 app.listen(port, () => console.log('Listening on port: ', port));
